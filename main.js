4d39331b5037569caedfd0bb94676745d7e4308d
aguscaceres99 <agustincaceres99@outlook.com>
2023-10-03 13:24:39 -0300

    segunda modificacion

--- main.js
+++ main.js
@@ -1,85 +1,85 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Inicializar variables
-    var saldo = 1000;
-    var productosDisponibles = {
-      productoA: 400,
-      productoB: 600,
-      productoC: 300,
-    };
-    var carrito = {};
-  
-    // Función para actualizar el saldo en la página
-    function actualizarSaldo() {
-      document.getElementById("saldo").innerText = saldo;
+  // Inicializar variables
+  let saldo = 1000;
+  let productosDisponibles = {
+    producto_a: 50,
+    producto_b: 30,
+    producto_c: 20,
+  };
+  let carrito = {};
+
+  // Función para actualizar el saldo en la página
+  function actualizarSaldo() {
+    document.getElementById("saldo").innerText = saldo;
+  }
+
+  // Función para mostrar los productos disponibles en la página
+  function mostrarProductos() {
+    const listaProductos = document.getElementById("productos");
+    listaProductos.innerHTML = "<h2>Productos disponibles:</h2>";
+
+    for (const producto in productosDisponibles) {
+      const item = document.createElement("li");
+      item.innerText = producto + " - $" + productosDisponibles[producto];
+      listaProductos.appendChild(item);
     }
-  
-    // Función para mostrar los productos disponibles en la página
-    function mostrarProductos() {
-      var listaProductos = document.getElementById("productos");
-      listaProductos.innerHTML = "<h2>Productos disponibles:</h2>";
-  
-      for (var producto in productosDisponibles) {
-        var item = document.createElement("li");
-        item.innerText = producto + " - $" + productosDisponibles[producto];
-        listaProductos.appendChild(item);
-      }
+  }
+
+  // Función para agregar un producto al carrito
+  function agregarAlCarrito() {
+    const productoInput = document.getElementById("productoInput").value.toLowerCase(); // Convertir a minúsculas
+    const precio = productosDisponibles[productoInput];
+
+    if (precio === undefined) {
+      alert("Producto no válido. Por favor, elige un producto de la lista.");
+      return;
     }
-  
-    // Función para agregar un producto al carrito
-    function agregarAlCarrito() {
-      var productoInput = document.getElementById("productoInput").value;
-      var precio = productosDisponibles[productoInput];
-  
-      if (precio === undefined) {
-        alert("Producto no válido. Por favor, elige un producto de la lista.");
-        return;
-      }
-  
-      if (saldo >= precio) {
-        saldo -= precio;
-  
-        if (carrito[productoInput]) {
-          carrito[productoInput]++;
-        } else {
-          carrito[productoInput] = 1;
-        }
-  
-        actualizarSaldo();
-        actualizarCarrito();
+
+    if (saldo >= precio) {
+      saldo -= precio;
+
+      if (carrito[productoInput]) {
+        carrito[productoInput]++;
       } else {
-        alert("No tienes suficiente saldo para comprar ese producto.");
+        carrito[productoInput] = 1;
       }
+
+      actualizarSaldo();
+      actualizarCarrito();
+    } else {
+      alert("No tienes suficiente saldo para comprar ese producto.");
     }
-  
-    // Función para actualizar el carrito en la página
-    function actualizarCarrito() {
-      var listaCarrito = document.getElementById("carrito");
-      listaCarrito.innerHTML = "<h2>Carrito de Compras:</h2>";
-  
-      for (var producto in carrito) {
-        var item = document.createElement("li");
-        item.innerText = producto + " x" + carrito[producto];
-        listaCarrito.appendChild(item);
-      }
+  }
+
+  // Función para actualizar el carrito en la página
+  function actualizarCarrito() {
+    const listaCarrito = document.getElementById("carrito");
+    listaCarrito.innerHTML = "<h2>Carrito de Compras:</h2>";
+
+    for (const producto in carrito) {
+      const item = document.createElement("li");
+      item.innerText = producto + " x" + carrito[producto];
+      listaCarrito.appendChild(item);
     }
-  
-    // Función para finalizar la compra
-    function finalizarCompra() {
-      var resumenCompra = "Resumen de compra:\n";
-  
-      for (var producto in carrito) {
-        resumenCompra += producto + ": " + carrito[producto] + " unidades\n";
-      }
-  
-      resumenCompra += "Saldo restante: $" + saldo + "\n¡Gracias por tu compra!";
-      alert(resumenCompra);
+  }
+
+  // Función para finalizar la compra
+  function finalizarCompra() {
+    let resumenCompra = "Resumen de compra:\n";
+
+    for (const producto in carrito) {
+      resumenCompra += producto + ": " + carrito[producto] + " unidades\n";
     }
-  
-    // Agregar event listeners a los botones
-    document.getElementById("agregarAlCarrito").addEventListener("click", agregarAlCarrito);
-    document.getElementById("finalizarCompra").addEventListener("click", finalizarCompra);
-  
-    // Inicializar la página
-    actualizarSaldo();
-    mostrarProductos();
-  });
+
+    resumenCompra += "Saldo restante: $" + saldo + "\n¡Gracias por tu compra!";
+    alert(resumenCompra);
+  }
+
+  // Agregar event listeners a los botones
+  document.getElementById("agregarAlCarrito").addEventListener("click", agregarAlCarrito);
+  document.getElementById("finalizarCompra").addEventListener("click", finalizarCompra);
+
+  // Inicializar la página
+  actualizarSaldo();
+  mostrarProductos();
+});
