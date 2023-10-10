4e4f9ece7ec150369ebc97d4426c6db97ced81f6
aguscaceres99 <agustincaceres99@outlook.com>
2023-10-10 14:44:00 -0300

    agregando algo de css y mas java

--- main.js
+++ main.js
@@ -1,85 +1,94 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Inicializar variables
-  let saldo = 1000;
-  let productosDisponibles = {
-    producto_a: 50,
-    producto_b: 30,
-    producto_c: 20,
-  };
-  let carrito = {};
+// Variables para el saldo y el carrito
+let saldo = 1000;
+let carrito = {};
 
-  // Función para actualizar el saldo en la página
-  function actualizarSaldo() {
-    document.getElementById("saldo").innerText = saldo;
-  }
+// Base de datos de productos disponibles como un array de objetos
+let productosDisponibles = [
+    { nombre: "Producto A", precio: 50 },
+    { nombre: "Producto B", precio: 30 },
+    { nombre: "Producto C", precio: 20 },
+];
 
-  // Función para mostrar los productos disponibles en la página
-  function mostrarProductos() {
-    const listaProductos = document.getElementById("productos");
-    listaProductos.innerHTML = "<h2>Productos disponibles:</h2>";
+// Referencias a elementos HTML
+const saldoElement = document.getElementById("saldo");
+const productosElement = document.getElementById("productos");
+const carritoElement = document.getElementById("carrito");
+const productoInput = document.getElementById("productoInput");
+const agregarAlCarritoButton = document.getElementById("agregarAlCarrito");
+const finalizarCompraButton = document.getElementById("finalizarCompra");
 
-    for (const producto in productosDisponibles) {
-      const item = document.createElement("li");
-      item.innerText = producto + " - $" + productosDisponibles[producto];
-      listaProductos.appendChild(item);
+// Función para actualizar el saldo en la página
+function actualizarSaldo() {
+    saldoElement.textContent = saldo;
+}
+
+// Función para mostrar los productos disponibles en la página
+function mostrarProductos() {
+    productosElement.innerHTML = "<h2>Productos disponibles:</h2>";
+
+    productosDisponibles.forEach((producto, index) => {
+        const item = document.createElement("li");
+        item.innerText = `${producto.nombre} - $${producto.precio}`;
+        productosElement.appendChild(item);
+    });
+}
+
+// Función para agregar un producto al carrito
+function agregarAlCarrito() {
+    const nombreProducto = productoInput.value;
+    const productoEncontrado = productosDisponibles.find((producto) => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
+
+    if (!productoEncontrado) {
+        alert("Producto no válido. Por favor, elige un producto de la lista.");
+        return;
     }
-  }
 
-  // Función para agregar un producto al carrito
-  function agregarAlCarrito() {
-    const productoInput = document.getElementById("productoInput").value.toLowerCase(); // Convertir a minúsculas
-    const precio = productosDisponibles[productoInput];
-
-    if (precio === undefined) {
-      alert("Producto no válido. Por favor, elige un producto de la lista.");
-      return;
-    }
+    const precio = productoEncontrado.precio;
 
     if (saldo >= precio) {
-      saldo -= precio;
+        saldo -= precio;
 
-      if (carrito[productoInput]) {
-        carrito[productoInput]++;
-      } else {
-        carrito[productoInput] = 1;
-      }
+        if (carrito[nombreProducto]) {
+            carrito[nombreProducto]++;
+        } else {
+            carrito[nombreProducto] = 1;
+        }
 
-      actualizarSaldo();
-      actualizarCarrito();
+        actualizarSaldo();
+        actualizarCarrito();
     } else {
-      alert("No tienes suficiente saldo para comprar ese producto.");
+        alert("No tienes suficiente saldo para comprar ese producto.");
     }
-  }
+}
 
-  // Función para actualizar el carrito en la página
-  function actualizarCarrito() {
-    const listaCarrito = document.getElementById("carrito");
-    listaCarrito.innerHTML = "<h2>Carrito de Compras:</h2>";
+// Función para actualizar el carrito en la página
+function actualizarCarrito() {
+    carritoElement.innerHTML = "<h2>Carrito de Compras:</h2>";
 
-    for (const producto in carrito) {
-      const item = document.createElement("li");
-      item.innerText = producto + " x" + carrito[producto];
-      listaCarrito.appendChild(item);
+    for (const nombreProducto in carrito) {
+        const item = document.createElement("li");
+        item.innerText = `${nombreProducto} x${carrito[nombreProducto]}`;
+        carritoElement.appendChild(item);
     }
-  }
+}
 
-  // Función para finalizar la compra
-  function finalizarCompra() {
+// Función para finalizar la compra
+function finalizarCompra() {
     let resumenCompra = "Resumen de compra:\n";
 
-    for (const producto in carrito) {
-      resumenCompra += producto + ": " + carrito[producto] + " unidades\n";
+    for (const nombreProducto in carrito) {
+        resumenCompra += `${nombreProducto}: ${carrito[nombreProducto]} unidades\n`;
     }
 
-    resumenCompra += "Saldo restante: $" + saldo + "\n¡Gracias por tu compra!";
+    resumenCompra += `Saldo restante: $${saldo}\n¡Gracias por tu compra!`;
     alert(resumenCompra);
-  }
+}
+// Agregar event listener al botón "Agregar al carrito"
+document.getElementById("agregarAlCarrito").addEventListener("click", function () {
+  agregarAlCarrito();
+});
 
-  // Agregar event listeners a los botones
-  document.getElementById("agregarAlCarrito").addEventListener("click", agregarAlCarrito);
-  document.getElementById("finalizarCompra").addEventListener("click", finalizarCompra);
-
-  // Inicializar la página
-  actualizarSaldo();
-  mostrarProductos();
+// Agregar event listener al botón "Finalizar compra"
+document.getElementById("finalizarCompra").addEventListener("click", function () {
+  finalizarCompra();
 });
